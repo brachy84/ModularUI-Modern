0c4d644698c0872290c226b70d9e45a5ca45b8b6
screret <68943070+screret@users.noreply.github.com>
2026-02-05 20:51:26 +0200

    Implement automatic atlasing for textures in /gui/
    This also supports mcmeta animations as a "byproduct" (the effect I want out of this)

--- src/main/java/com/cleanroommc/modularui/client/ModularUIClient.java
+++ src/main/java/com/cleanroommc/modularui/client/ModularUIClient.java
@@ -5,8 +5,10 @@ import com.cleanroommc.modularui.ModularUIMenuTypes;
 import com.cleanroommc.modularui.screen.ContainerScreenWrapper;
 import com.cleanroommc.modularui.screen.ModularContainerMenu;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -18,5 +20,10 @@ public class ModularUIClient {
     public static void registerScreens(FMLClientSetupEvent event) {
         //noinspection deprecation
         event.enqueueWork(() -> MenuScreens.<ModularContainerMenu, ContainerScreenWrapper>register(ModularUIMenuTypes.MODULAR_CONTAINER.get(), ContainerScreenWrapper::new));
+    }
+
+    @SubscribeEvent
+    public static void registerClientReloadListeners(RegisterClientReloadListenersEvent event) {
+        event.registerReloadListener(new GuiSpriteManager(Minecraft.getInstance().textureManager));
     }
 }
