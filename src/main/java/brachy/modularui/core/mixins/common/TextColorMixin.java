fc47917419da08599efdeb41e379f8111cf74ea6
brachy84 <brachy84@gmail.com>
2026-05-18 16:40:03 +0200

    Redirect instead of Inject

--- src/main/java/brachy/modularui/core/mixins/common/TextColorMixin.java
+++ src/main/java/brachy/modularui/core/mixins/common/TextColorMixin.java
@@ -4,8 +4,7 @@ import net.minecraft.network.chat.TextColor;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TextColor.class)
 public class TextColorMixin {
@@ -13,12 +12,8 @@ public class TextColorMixin {
     /**
      * @reason Minecraft uses Integer.parseInt which fails when alpha is specified because of signed vs. unsigned
      */
-    @Inject(method = "parseColor", at = @At(value = "INVOKE", target = "Ljava/lang/Integer;parseInt(Ljava/lang/String;I)I"), cancellable = true)
-    private static void fixDecode(String hexString, CallbackInfoReturnable<TextColor> cir) {
-        try {
-            cir.setReturnValue(TextColor.fromRgb((int) (long) Long.decode(hexString)));
-        } catch (NumberFormatException e) {
-            cir.setReturnValue(null);
-        }
+    @Redirect(method = "parseColor", at = @At(value = "INVOKE", target = "Ljava/lang/Integer;parseInt(Ljava/lang/String;I)I"))
+    private static int fixDecode(String s, int radix) {
+        return (int) Long.parseLong(s, 16);
     }
 }
