9cc28b720ebac29c4749c84e9b880bd479bad156
jurrejelle <jurre@jilles.com>
2026-05-27 18:44:33 +0200

    Reorder

--- src/main/java/brachy/modularui/utils/serialization/network/ByteBufAdapters.java
+++ src/main/java/brachy/modularui/utils/serialization/network/ByteBufAdapters.java
@@ -43,11 +43,11 @@ public class ByteBufAdapters {
 
     public static final IByteBufAdapter<BlockState> BLOCKSTATE = makeAdapterFromCodec(BlockState.CODEC, BlockState::equals);
     public static final IByteBufAdapter<BlockPos> BLOCKPOS = makeAdapterFromCodec(BlockPos.CODEC, BlockPos::equals);
+    public static final IByteBufAdapter<GlobalPos> GLOBAL_POS = makeAdapterFromCodec(GlobalPos.CODEC, GlobalPos::equals);
+    public static final IByteBufAdapter<ResourceLocation> RESOURCE_LOCATION = makeAdapterFromCodec(ResourceLocation.CODEC, ResourceLocation::equals);
+    public static final IByteBufAdapter<UUID> UUID = makeAdapter(FriendlyByteBuf::readUUID, FriendlyByteBuf::writeUUID, java.util.UUID::equals);
     public static final IByteBufAdapter<Component> COMPONENT = makeAdapter(FriendlyByteBuf::readComponent, FriendlyByteBuf::writeComponent,
             (a, b) -> Objects.equals(a.toString(), b.toString()));
-    public static final IByteBufAdapter<GlobalPos> GLOBAL_POS = makeAdapterFromCodec(GlobalPos.CODEC, GlobalPos::equals);
-    public static final IByteBufAdapter<UUID> UUID = makeAdapter(FriendlyByteBuf::readUUID, FriendlyByteBuf::writeUUID, java.util.UUID::equals);
-    public static final IByteBufAdapter<ResourceLocation> RESOURCE_LOCATION = makeAdapterFromCodec(ResourceLocation.CODEC, ResourceLocation::equals);
 
     public static final IByteBufAdapter<byte[]> BYTE_ARR = new IByteBufAdapter<>() {
 
