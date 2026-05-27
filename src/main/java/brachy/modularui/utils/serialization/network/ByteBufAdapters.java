83648defbdaf8887fef663c26b6fb10d699ea734
jurrejelle <jurre@jilles.com>
2026-05-27 18:47:56 +0200

    toString -> getString

--- src/main/java/brachy/modularui/utils/serialization/network/ByteBufAdapters.java
+++ src/main/java/brachy/modularui/utils/serialization/network/ByteBufAdapters.java
@@ -47,7 +47,7 @@ public class ByteBufAdapters {
     public static final IByteBufAdapter<ResourceLocation> RESOURCE_LOCATION = makeAdapterFromCodec(ResourceLocation.CODEC, ResourceLocation::equals);
     public static final IByteBufAdapter<UUID> UUID = makeAdapter(FriendlyByteBuf::readUUID, FriendlyByteBuf::writeUUID, java.util.UUID::equals);
     public static final IByteBufAdapter<Component> COMPONENT = makeAdapter(FriendlyByteBuf::readComponent, FriendlyByteBuf::writeComponent,
-            (a, b) -> Objects.equals(a.toString(), b.toString()));
+            (a, b) -> Objects.equals(a.getString(), b.getString()));
 
     public static final IByteBufAdapter<byte[]> BYTE_ARR = new IByteBufAdapter<>() {
 
