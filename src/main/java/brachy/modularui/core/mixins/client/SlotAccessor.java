713bc967143eeb21bbf3af3bccb64de2bfec20e3
Ghostipedia <Ghostipedia@gmail.com>
2026-07-01 15:30:52 -0400

    Mirror the 1.20 Slot Accessor Fix

--- src/main/java/brachy/modularui/core/mixins/client/SlotAccessor.java
+++ src/main/java/brachy/modularui/core/mixins/client/SlotAccessor.java
@@ -12,11 +12,11 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(Slot.class)
 public interface SlotAccessor {
 
-    @Accessor
+    @Accessor("x")
     @Mutable
     void setX(int x);
 
-    @Accessor
+    @Accessor("y")
     @Mutable
     void setY(int y);
 }
